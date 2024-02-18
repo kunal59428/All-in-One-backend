fb4f4dd0a69b24ad170a59978926474e18422cd3
kunal59428 <kumarsharmakunal@gmail.com>
2024-02-18 23:29:20 +0530

    updated

--- Router/consumerRoute.js
+++ Router/consumerRoute.js
@@ -3,10 +3,8 @@ const { consumer_register, consumer_login } = require("../Controller/consumerCon
 const consumerRouter = require("express").Router()
 
 
-consumerRouter.post("/register",consumer_register)
+consumerRouter.post("/register", consumer_register)
 
 consumerRouter.post("/login", consumer_login)
-
-consumerRouter.get("/login", consumer_login)
 
 module.exports = consumerRouter
